cf614ac0aebf5979b0027814834359b4a2ec83e9
parsa <parsamesg@gmail.com>
2021-10-30 15:33:24 +0100

    add spotify now playing (took a long time)

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
-import Date from "../components/Date";
-import Layout, { siteTitle } from "../components/Layout";
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
+import responseHandler from "./api/now-playing";
 
 export const getStaticProps = async () => {
   const allPostsData = getSortedPostsData();
+  const songData = await responseHandler();
   return {
     props: {
       allPostsData,
+      songData,
     },
   };
 };
 
-const Home = ({ allPostsData }) => {
+const Home = ({ allPostsData, songData }) => {
   return (
     <Layout home>
       <Head>
@@ -26,6 +30,23 @@ const Home = ({ allPostsData }) => {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        {songData?.isPlaying ? (
+          <>
+            <Image
+              priority
+              src={songData.albumImageUrl}
+              height={250}
+              width={250}
+              alt={songData.album}
+              title={songData.album}
+            />
+            <p>
+              {songData.title} by {songData.artist}
+            </p>
+          </>
+        ) : (
+          <p>not playing right now</p>
+        )}
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
